4c8acc17b5bba5ad71f99a83f1275f9911f840d9
Anurag Maldhure <hello.anurag.maldhure@gmail.com>
2023-11-22 22:38:58 +0530

    Assignment 4 Que 5 resolved

---
+++ Assignment_4_Question_5/src/com/ll/Main.java
@@ -0,0 +1,19 @@
+package com.ll;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SLLL list = new SLLL();
+
+		list.addNode(10);
+		list.addNode(20);
+		list.addNode(15);
+		list.addNode(6);
+		
+		list.display();
+		
+	}
+
+}
