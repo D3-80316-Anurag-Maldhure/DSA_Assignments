249a5fabb2afa1cf8d17f08da24f39aae5d18798
Anurag Maldhure <hello.anurag.maldhure@gmail.com>
2023-11-22 18:16:43 +0530

    Assignment 6 Que 3 done

---
+++ Assignment_6_Question_3/src/com/bst/BinarySearchTree.java
@@ -0,0 +1,162 @@
+package com.bst;
+
+public class BinarySearchTree {
+	static class Node{
+		private int data;
+		private Node left;
+		private Node right;
+		public Node(int value) {
+			data = value;
+			left = null;
+			right = null;
+		}
+		public int getData() {
+			return data;
+		}
+	}
+	
+	private Node root;
+	public BinarySearchTree() {
+		root = null;
+	}
+	
+	public int countNonLeafNodes() {
+	    if (isEmpty() || root == null) {
+	        return 0;
+	    } else {
+	        return countNonLeafRecursively(root);
+	    }
+	}
+
+	public int countNonLeafRecursively(Node trav) {
+	    if (trav == null || (trav.left == null && trav.right == null)) {
+	        return 0; // Base case: null node or leaf node
+	    }
+
+	    // Non-leaf node, so count it and recursively count its children
+	    int leftCount = countNonLeafRecursively(trav.left);
+	    int rightCount = countNonLeafRecursively(trav.right);
+
+	    // Return the sum of non-leaf counts in both subtrees
+	    return 1 + leftCount + rightCount;
+	}
+
+	
+//	public int countLeafNodes() {
+//	    if (isEmpty()) {
+//	        return 0;
+//	    } else {
+//	        return countLeafRecursively(root);
+//	    }
+//	}
+//
+//	public int countLeafRecursively(Node trav) {
+//	    if (trav == null) {
+//	        return 0;
+//	    }
+//
+//	    // If the current node is a leaf, return 1
+//	    if (trav.left == null && trav.right == null) {
+//	    	//System.out.println(trav.data);
+//	        return 1;
+//	    }
+//
+//	    // Recursive calls for left and right subtrees
+//	    int leftCount = countLeafRecursively(trav.left);
+//	    int rightCount = countLeafRecursively(trav.right);
+//
+//	    // Return the sum of leaf counts in both subtrees
+//	    return leftCount + rightCount;
+//	}
+
+	
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	public void addNode(int value) {
+		//1. create node with given data
+		Node newnode = new Node(value);
+		//2. if tree is empty
+		if(isEmpty())
+			//a. add newnode into root
+			root = newnode;
+		//3. if tree is not empty
+		else {
+			//3.1 create trave and start at root
+			Node trav = root;
+			while(true) {
+				//3.2 if value is less than current node data
+				if(value  < trav.data) {
+					//3.2.1 if left is empty
+					if(trav.left == null) {
+						// add newnode into left of current node
+						trav.left = newnode;
+						break;
+					}
+					//3.2.2 if left is not empty
+					else
+						// go into left
+						trav = trav.left;
+				}
+				//3.3 if value is greater than current node data
+				else {
+					//3.2.1 if right is empty
+					if(trav.right == null) {
+						// add newnode into right of current node
+						trav.right = newnode;
+						break;
+					}
+					//3.2.2 if right is not empty
+					else
+						// go into right
+						trav = trav.right;
+				}
+			}//3.4 repeat step 3.2 and 3.3 till node is not added into tree
+		}
+	}
+	
+	public void preorder(Node trav) {
+		if(trav == null)
+			return;
+		System.out.print(" " + trav.data);//V 
+		preorder(trav.left);//L 
+		preorder(trav.right);//R
+	}
+	
+	public void preorder() {
+		System.out.print("Preorder: ");
+		preorder(root);
+		System.out.println("");
+	}
+	
+	public void inorder(Node trav) {
+		if(trav == null)
+			return;
+		inorder(trav.left);//L 
+		System.out.print(" " + trav.data);//V 
+		inorder(trav.right);//R
+	}
+	
+	public void inorder() {
+		System.out.print("Inorder: ");
+		inorder(root);
+		System.out.println("");
+	}
+	
+	public void postorder(Node trav) {
+		if(trav == null)
+			return;
+		postorder(trav.left);//L 
+		postorder(trav.right);//R
+		System.out.print(" " + trav.data);//V
+	}
+	
+	public void postorder() {
+		System.out.print("Postorder: ");
+		postorder(root);
+		System.out.println("");
+	}
+	
+}
+
